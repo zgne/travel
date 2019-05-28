3229dd42a7559c18ac393892ddf825dda2afece2
ge zhu <1169264363@qq.com>
2019-05-28 21:38:14 +0800

    a vue.js of travel

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="home">
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+  import HomeHeader from './components/Header'
+  export default {
+    name: 'Home',
+    components:{
+      HomeHeader,
+    }
+  }
+</script>
+
+<style></style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont back-icon">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      请输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeHeader',
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles'
+  .header
+    color: #ffffff
+    display: flex
+    height: .86rem
+    line-height: .86rem
+    background: #00bcd4
+    .header-left
+      float: left
+      width: .64rem
+      text-align: center
+      .back-icon
+        font-size: .4rem
+    .header-input
+      flex-grow: 1
+      color: #ccc
+      height: .64rem
+      line-height: .64rem
+      margin-top: .11rem
+      margin-left: .2rem
+      padding-left: .2rem
+      border-radius: .1rem
+      background-color: #fff
+    .header-right
+      float: right
+      width: 1.24rem
+      text-align: center
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
+
+</style>
