5690336af290e095a3e91a96d3e18496694bc17a
ge zhu <1169264363@qq.com>
2019-06-13 17:06:43 +0800

    remove event

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -36,6 +36,9 @@
     },
     activated(){
       window.addEventListener('scroll',this.handleScroll)
+    },
+    deactivated() {
+      window.removeEventListener("scroll",this.handleScroll)
     }
   }
 </script>
